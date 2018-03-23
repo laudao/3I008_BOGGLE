52c3fdfd095d791429e65485a8d079697a7005f6
Fati <reihaneh77@yahoo.com>
2018-03-23 19:54:28 +0100

    suffix compiled add changed but problems while compiling

--- boggle/lib/lexicon.ml
+++ boggle/lib/lexicon.ml
@@ -20,52 +20,49 @@ let empty =
   }
 
 let is_empty { eow; words } =
-  if words=empty.words then true else false
+    failwith "Unimplemented"
+(*eow==false && M.for_all empty words*)
 
 (*let rec is_empty { eow; words } =
   if words=empty.words then true else false*)
 
-(*
-let has_empty_word { eow; words } =
-  Iter.filter (fun x -> if x=true then true else false )(Iter.map (fun x -> is_empty { false; x }) M.to_iter words)
-*)
 
-  (*match (eow,words) with
+let has_empty_word { eow; words } =
+  failwith "Unimplemented"
+(*Iter.filter (fun x -> if x=true then true else false )(Iter.map (fun x -> is_empty { false; x }) M.to_iter words
+
+ match (eow,words) with
   |(false,words) when is_empty {eow; words} = true -> true
   |(false,words) -> has_empty_word {words.eow; words}
   |(true,_)->false*)
 
 let add word lexicon =
-  let rec traverse n t =
+	let rec traverse n t =
     if n < String.length word then
-      match t with
-		  	| empty -> {eow= (n=(String.length word)-1); words=(traverse (n+1) empty)}
-        | {eow=false; words=dict} when exists (fun (c,tr) -> c = word.[n]) dict ->
-            traverse (n+1) (find word.[n] dict)
-        | {eow=b; words=dict} -> {eow=false; words= add word.[n] (traverse (n+1) empty)}
+		if M.mem word.[n] t.words then
+			{eow=t.eow;words=(traverse (n+1) (M.find word.[n] t.words))}
+		else
+			{eow=t.eow;words=(traverse (n+1) (M.add word.[n] empty (t.words)))}
+	else 
+		{eow=true;words=t.words}
+ in traverse 0 lexicon
 
-	  else empty
-  in traverse 0 lexicon
-
-(*
-  let rec traverse n lex =
-    if n < String.length word then
-      let t = ((List.nth 0 (to_rev_list (Iter.map (fun (c,j) -> if word.[n]=c then (c;j) else (c,empty)) (M.to_iter dict.words)))))
-      match t with
-      | t when is_empty t ->
-
-      | t -> traverse (n+1) t
-    else
-  in traverse 0 lexicon*)
-
-let rec to_iter { eow; words } =
-  failwith "Unimplemented"
+let rec to_iter { eow; words } =  
+	failwith "Unimplemented"
 
 let letter_suffixes { eow; words } letter =
-  failwith "Unimplemented"
+	let rec traverse n t =
+		if M.mem letter t.words then
+			{eow=false;words=t.words}
+		else
+			empty
+ in traverse 0 	{ eow; words }
 
 let rec filter_min_length len { eow; words } =
   failwith "Unimplemented"
+  (* let mots = (Iter.filter (fun x -> if String.length(x) > len then true else false)(to_iter { eow; words })) in
+	let lex = empty in
+	Iter.map (fun x -> add x lex) mots *)
 
 let load_file f =
   let rec load_channel channel acc =
